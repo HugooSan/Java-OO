68a94496940baff9ac626577df09f34f853b6853
Hugo Shinde <82584511+HugooSan@users.noreply.github.com>
2021-08-18 21:19:46 -0300

    Classes e Metodos Abstratos
    Aulas de classes e métodos abstratos, uso das classes mães como abstratas e implementação dos métodos abstratos.

--- bytebank-herdado/src/TesteFuncionario.java
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -3,17 +3,16 @@ public class TesteFuncionario {
 	public static void main(String[] args) {
 		
 		
-		Gerente hugo = new Gerente();
+		Funcionario hugo = new Gerente();
 		hugo.setNome("Hugo Shinde");
 		hugo.setSalario(5000);
 		
-		Funcionario f = new Funcionario();
-		f.setSalario(2000);
+		Funcionario des = new Designer();
+		des.setSalario(5000);
 		
 		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(hugo);
-		controle.registra(f);
 		
 		System.out.println(controle.getSoma());
 		
